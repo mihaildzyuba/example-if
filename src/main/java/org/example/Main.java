15ecedcbe902ca78d66f8d418ac5452bd1c0c167
Mihail_Dz <monyasha1970@gmail.com>
2024-02-06 15:26:31 +0200

    implement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += i;
+
+        }
+        System.out.println("Sum:" +sum);
     }
 }
